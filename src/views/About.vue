8bb905ae853c5e410b5acf7476a47190d570a482
WeLa <laurin.weber@students.hevs.ch>
2020-04-05 14:45:40 +0200

    Guidelines & About-page with content

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,11 +14,42 @@
         <section class="section section-skew">
             <div class="container">
                 <card shadow class="card-profile mt--300" no-body>
+                    <h1 class="pt-2">How our tool works</h1>
                     <div class="px-4">
                         <div class="row justify-content-center">
                             <!-- content -->
                             <div class="col-lg-9">
+                                <div style="text-align: left">
+                                    <p>Our tool performs different analyses using a database created by our team.
+                                        This database contains articles with true and false information.
+                                        The following criteria were used for the article selection.</p>
+                                </div>
+                                <div style="text-align: left">
+                                <p>True news</p>
+                                </div>
 
+                                <ul class="list" style="text-align: left">
+                                    <li>Selection of articles (n= 140) was made in four categories :
+                                        <ul class="list" style="text-align: left">
+                                            <li>Government publications</li>
+                                            <li>Non-Government / Associations Reports and Publications</li>
+                                            <li>Industry / Private sector publications</li>
+                                            <li>Social Media</li>
+                                        </ul>
+                                    </li>
+                                </ul>
+
+                                <div style="text-align: left">
+                                    <p>False news</p>
+                                </div>
+
+                                    <ul class="list" style="text-align: left">
+                                    <li>To be completed
+                                        <ul class="list" style="text-align: left">
+                                            <li>Reasoning 1</li>
+                                        </ul>
+                                    </li>
+                                </ul>
 
                             </div>
                             <!-- end of content -->
